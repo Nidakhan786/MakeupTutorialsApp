9cbbd7ed377c45e76fc2633dfac576ef8ad12178
nidakhan786 <nidaslife23@gmail.com>
2019-11-11 17:59:54 +0530

    Updated Highlighter.dart

--- lib/Highlighter.dart
+++ lib/Highlighter.dart
@@ -3,6 +3,77 @@ import 'package:youtube_player_flutter/youtube_player_flutter.dart';
 
 
 class  highlighter extends StatelessWidget{
+  YoutubePlayerController _controller = YoutubePlayerController(
+    initialVideoId: '_jylrAXW82U',
+    flags: YoutubePlayerFlags(
+      autoPlay: true,
+      mute: true,
+    ),
+  );
+  YoutubePlayerController _controller1 = YoutubePlayerController(
+    initialVideoId: 'Hj78NqRWNC4',
+    flags: YoutubePlayerFlags(
+      autoPlay: true,
+      mute: true,
+    ),
+  );
+  YoutubePlayerController _controller2 = YoutubePlayerController(
+    initialVideoId: 'T7P4pGV5YxY',
+    flags: YoutubePlayerFlags(
+      autoPlay: true,
+      mute: true,
+    ),
+  );
+  YoutubePlayerController _controller3 = YoutubePlayerController(
+    initialVideoId: '5niudb1Ywr8',
+    flags: YoutubePlayerFlags(
+      autoPlay: true,
+      mute: true,
+    ),
+  );
+  YoutubePlayerController _controller4 = YoutubePlayerController(
+    initialVideoId: 'EaCvDjZi4o0',
+    flags: YoutubePlayerFlags(
+      autoPlay: true,
+      mute: true,
+    ),
+  );
+  YoutubePlayerController _controller5 = YoutubePlayerController(
+    initialVideoId: 'HY8zWtTUvEA',
+    flags: YoutubePlayerFlags(
+      autoPlay: true,
+      mute: true,
+    ),
+  );
+  YoutubePlayerController _controller6 = YoutubePlayerController(
+    initialVideoId: 'dkqbUer0Lfc',
+    flags: YoutubePlayerFlags(
+      autoPlay: true,
+      mute: true,
+    ),
+  );
+  YoutubePlayerController _controller7 = YoutubePlayerController(
+    initialVideoId: 'bcmwZnk9KzY',
+    flags: YoutubePlayerFlags(
+      autoPlay: true,
+      mute: true,
+    ),
+  );
+  YoutubePlayerController _controller8 = YoutubePlayerController(
+    initialVideoId: 'Z8TdCfawGQs',
+    flags: YoutubePlayerFlags(
+      autoPlay: true,
+      mute: true,
+    ),
+  );
+  YoutubePlayerController _controller9 = YoutubePlayerController(
+    initialVideoId: 'EjQv-2UKQk0',
+    flags: YoutubePlayerFlags(
+      autoPlay: true,
+      mute: true,
+    ),
+  );
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
@@ -18,20 +89,112 @@ class  highlighter extends StatelessWidget{
                 child: Column(
                   children: <Widget>[
                     new Text('Video 1'),
-
                     YoutubePlayer(
-                        context: context,
-                        videoId: "_jylrAXW82U",
-                        flags: YoutubePlayerFlags(
-                          autoPlay: false,
-                          showVideoProgressIndicator: true,
-                        )
-                    ),
-
+                      controller: _controller,
+                      showVideoProgressIndicator: true,
+                    )
                   ],
                 ),
-
-              )
+              ),
+              new Card(
+                child: Column(
+                  children: <Widget>[
+                    new Text('Video 1'),
+                    YoutubePlayer(
+                      controller: _controller1,
+                      showVideoProgressIndicator: true,
+                    )
+                  ],
+                ),
+              ),
+              new Card(
+                child: Column(
+                  children: <Widget>[
+                    new Text('Video 1'),
+                    YoutubePlayer(
+                      controller: _controller2,
+                      showVideoProgressIndicator: true,
+                    )
+                  ],
+                ),
+              ),
+              new Card(
+                child: Column(
+                  children: <Widget>[
+                    new Text('Video 1'),
+                    YoutubePlayer(
+                      controller: _controller3,
+                      showVideoProgressIndicator: true,
+                    )
+                  ],
+                ),
+              ),
+              new Card(
+                child: Column(
+                  children: <Widget>[
+                    new Text('Video 1'),
+                    YoutubePlayer(
+                      controller: _controller4,
+                      showVideoProgressIndicator: true,
+                    )
+                  ],
+                ),
+              ),
+              new Card(
+                child: Column(
+                  children: <Widget>[
+                    new Text('Video 1'),
+                    YoutubePlayer(
+                      controller: _controller5,
+                      showVideoProgressIndicator: true,
+                    )
+                  ],
+                ),
+              ),
+              new Card(
+                child: Column(
+                  children: <Widget>[
+                    new Text('Video 1'),
+                    YoutubePlayer(
+                      controller: _controller6,
+                      showVideoProgressIndicator: true,
+                    )
+                  ],
+                ),
+              ),
+              new Card(
+                child: Column(
+                  children: <Widget>[
+                    new Text('Video 1'),
+                    YoutubePlayer(
+                      controller: _controller7,
+                      showVideoProgressIndicator: true,
+                    )
+                  ],
+                ),
+              ),
+              new Card(
+                child: Column(
+                  children: <Widget>[
+                    new Text('Video 1'),
+                    YoutubePlayer(
+                      controller: _controller8,
+                      showVideoProgressIndicator: true,
+                    )
+                  ],
+                ),
+              ),
+              new Card(
+                child: Column(
+                  children: <Widget>[
+                    new Text('Video 1'),
+                    YoutubePlayer(
+                      controller: _controller9,
+                      showVideoProgressIndicator: true,
+                    )
+                  ],
+                ),
+              ),
 
             ],
           ),
